50ae90c5db702f125ae38820f9b0a72aff18ff68
Md Taufique <mdtaufique8084@gmail.com>
2025-07-22 18:18:55 +0530

    Time: 7 ms (95.23%) | Memory: 60.9 MB (39.66%) - LeetSync

--- 1813-maximum-erasure-value/maximum-erasure-value.java
+++ 1813-maximum-erasure-value/maximum-erasure-value.java
@@ -1,20 +1,22 @@
 class Solution {
     public int maximumUniqueSubarray(int[] nums) {
         int n=nums.length;
-        Set<Integer> set=new HashSet<>();
-        int sum=nums[0];
-        int maxSum=sum;
-        set.add(nums[0]);
-        int l=0;
-        for(int r=1;r<n;r++){
-            while(set.contains(nums[r])){
-                sum-=nums[l];
-                set.remove(nums[l]);
-                l++;
-            }
-            set.add(nums[r]);
-            sum+=nums[r];
-            maxSum=Math.max(maxSum,sum);
+        int[] prefix=new int[n];
+        prefix[0]=nums[0];
+        for(int i=1;i<n;i++){
+            prefix[i]=prefix[i-1]+nums[i];
+        }
+        int i=0,j=0;
+        int maxSum=0;
+        int[] map=new int[(int)1e4+1];
+        Arrays.fill(map,-1);
+        while(j<n){
+            i=Math.max(i,map[nums[j]]+1);
+            int jthSum=prefix[j];
+            int ithSum=i-1<0?0:prefix[i-1];
+            maxSum=Math.max(maxSum,jthSum-ithSum);
+            map[nums[j]]=j;
+            j++;
         }
         return maxSum;
     }
