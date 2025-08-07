7edf9136d51084c8ba9a407148d63eb71d474e38
Md Taufique <mdtaufique8084@gmail.com>
2025-08-07 21:05:20 +0530

    Time: 32 ms (50%), Space: 197.8 MB (55%) - LeetHub

--- 3648-find-the-maximum-number-of-fruits-collected/3648-find-the-maximum-number-of-fruits-collected.java
+++ 3648-find-the-maximum-number-of-fruits-collected/3648-find-the-maximum-number-of-fruits-collected.java
@@ -1,81 +1,66 @@
 class Solution {
     int n;
-    int[][] memo;
-
-    // row = i, col = j
-    int dfs3(int row, int col, int[][] mat) {
-        if (row < 0 || col < 0 || row >= n || col >= n) return 0;
-
-        if (memo[row][col] != -1) return memo[row][col];
-
-        int val = mat[row][col];
-        int res = 0;
-
-        if (row == col) {
-            res = Math.max(res, dfs3(row + 1, col + 1, mat));
-        } else if (row - 1 == col) {
-            res = Math.max(res, Math.max(
-                dfs3(row + 1, col + 1, mat),
-                dfs3(row, col + 1, mat)
-            ));
-        } else {
-            res = Math.max(res, Math.max(
-                dfs3(row + 1, col + 1, mat),
-                Math.max(dfs3(row, col + 1, mat),
-                         dfs3(row - 1, col + 1, mat))
-            ));
+    int[][] dp;
+    public int maxCollectedFruits(int[][] fruits) {
+        n = fruits.length;
+        dp=new int[n][n];
+        for(int[] d:dp){
+            Arrays.fill(d,-1);
         }
 
-        return memo[row][col] = val + res;
+        int sum1 = solve1(0,fruits);
+        int sum2 = solve2(0, n - 1, fruits);
+        int sum3 = solve3(n - 1, 0, fruits);
+        return sum1 + sum2 + sum3;
     }
 
-    int dfs2(int row, int col, int[][] mat) {
-        if (row < 0 || col < 0 || row >= n || col >= n) return 0;
-
-        if (memo[row][col] != -1) return memo[row][col];
-
-        int val = mat[row][col];
+    private int solve1(int i, int[][] fruits) {
         int res = 0;
-
-        if (row == col) {
-            res = Math.max(res, dfs2(row + 1, col + 1, mat));
-        } else if (row == col - 1) {
-            res = Math.max(res, Math.max(
-                dfs2(row + 1, col + 1, mat),
-                dfs2(row + 1, col, mat)
-            ));
-        } else {
-            res = Math.max(res, Math.max(
-                dfs2(row + 1, col + 1, mat),
-                Math.max(dfs2(row + 1, col, mat),
-                         dfs2(row + 1, col - 1, mat))
-            ));
+        for (i = 0; i < n; i++) {
+            res += fruits[i][i];
+            fruits[i][i]=0;
+            dp[i][i]=0;
         }
-
-        return memo[row][col] = val + res;
+        return res;
     }
 
-    public int maxCollectedFruits(int[][] mat) {
-        n = mat.length;
-        int total = 0;
-
-        memo = new int[n][n];
-        for (int[] row : memo) {
-            Arrays.fill(row, -1);
+    private int solve2(int i, int j, int[][] fruits) {
+        if (i < 0 || i >= n || j < 0 || j >= n) {
+            return 0;
         }
-
-        // child - 1
-        // he will eat all diagonal fruits, so set them to 0 
-        for (int i = 0; i < n; i++) {
-            total += mat[i][i];
-            mat[i][i] = 0;
+        if(i==n-1 && j==n-1){
+            return 0;
         }
+        if(i==j || i>j){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int p1 =solve2(i + 1, j - 1, fruits);
+        int p2 =solve2(i + 1, j, fruits);
+        int p3 =solve2(i + 1, j + 1, fruits);
+        int result = fruits[i][j] + Math.max(p1, Math.max(p2, p3));
+        return dp[i][j]=result;
+    }
 
-        // child - 2
-        total += dfs3(n - 1, 0, mat);
-        // child - 3
-        total += dfs2(0, n - 1, mat);
-
-        return total;
+    private int solve3(int i, int j, int[][] fruits) {
+        if (i < 0 || j < 0 || i >= n || j >= n) {
+            return 0;
+        }
+        if(i==n-1 && j==n-1){
+            return 0;
+        }
+        if(i==j || j>i){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int p1 =solve3(i - 1, j + 1, fruits);
+        int p2 =solve3(i, j + 1, fruits);
+        int p3 =solve3(i + 1, j + 1, fruits);
+        int result =fruits[i][j] +  Math.max(p1, Math.max(p2, p3));
+        return dp[i][j]= result;
     }
 }
