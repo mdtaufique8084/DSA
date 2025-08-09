6817e52efd61a7fe18dd650bd1f95901a6c06e40
Md Taufique <mdtaufique8084@gmail.com>
2025-08-09 14:58:00 +0530

    Time: 1 ms (89.09%) | Memory: 41.6 MB (85.45%) - LeetSync

---
+++ 826-soup-servings/soup-servings.java
@@ -0,0 +1,37 @@
+class Solution {
+    public double soupServings(int n) {
+        if (n > 5000) return 1.0; // Optimization for large n
+        // Scale down to multiples of 25
+        n = (int) Math.ceil(n / 25.0);
+        double[][] serve = {
+            {4, 0}, {3, 1}, {2, 2}, {1, 3} // scaled down servings
+        };
+        double[][] dp=new double[n+1][n+1];
+        for(double d[]:dp){
+            Arrays.fill(d,-1.0);
+        }
+        return solve(n, n, serve,dp);
+    }
+
+    private double solve(int a, int b, double[][] serve,double[][] dp) {
+        if (a <= 0 && b <= 0) {
+            return 0.5;
+        }
+        if (a <= 0) {
+            return 1.0;
+        }
+        if (b <= 0) {
+            return 0.0;
+        }
+        if(dp[a][b]!=-1.0){
+            return dp[a][b];
+        }
+        double probability = 0.0;
+        for (double[] ser : serve) {
+            int a_s = (int) ser[0];
+            int b_s = (int) ser[1];
+            probability += 0.25 * solve(a - a_s, b - b_s, serve,dp);
+        }
+        return dp[a][b]= probability;
+    }
+}
