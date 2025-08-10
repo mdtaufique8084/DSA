9cecfa63b6349f5041d4c47ccbce059e7275be29
Md Taufique <mdtaufique8084@gmail.com>
2025-08-10 10:55:39 +0530

    Time: 3 ms (14.21%), Space: 41.3 MB (22.63%) - LeetHub

---
+++ 0900-reordered-power-of-2/0900-reordered-power-of-2.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean reorderedPowerOf2(int n) {
+        int[] digits = numToArray(n);
+        int len = digits.length;
+        HashMap<Integer, Integer> map1 = new HashMap<>();
+        for (int digit : digits) {
+            map1.put(digit, map1.getOrDefault(digit, 0) + 1);
+        }
+        for (int i = 1; i <= 1_000_000_000; i *= 2) {
+            int[] newDigits = numToArray(i);
+            HashMap<Integer, Integer> map2 = new HashMap<>();
+            for (int digit : newDigits) {
+                map2.put(digit, map2.getOrDefault(digit, 0) + 1);
+            }
+            if(map1.equals(map2)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private int[] numToArray(int num) {
+        int temp = num;
+        int length = length(temp);
+        int[] digits = new int[length];
+        for (int i = length - 1; i >= 0; i--) {
+            digits[i] = num % 10;
+            num /= 10;
+        }
+        return digits;
+    }
+
+    private int length(int temp){
+        int length=0;
+        while(temp>0){
+            length++;
+            temp=temp/10;
+        }
+        return length;
+    }
+}
