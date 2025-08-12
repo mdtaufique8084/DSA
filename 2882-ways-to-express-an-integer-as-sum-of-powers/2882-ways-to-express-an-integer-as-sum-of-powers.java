d9b7f6dc6decf3fd89a835b345d023fd8647be21
Md Taufique <mdtaufique8084@gmail.com>
2025-08-12 18:52:53 +0530

    Time: 470 ms (12.63%), Space: 45 MB (43.68%) - LeetHub

--- 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.java
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.java
@@ -2,36 +2,29 @@ class Solution {
     int[][] dp;
     int mod=(int)(1e9+7);
     public int numberOfWays(int n, int x) {
-        int[] arr=new int[n];
-        for(int i=0;i<n;i++){
-            arr[i]=i+1;
-        }
-        int[] power=new int[n];
-        for(int i=0;i<n;i++){
-            power[i]=(int)Math.pow(arr[i],x);
-        }
-        dp=new int[n][n+1];
+        dp=new int[n+1][n+1];
         for(int[] d:dp){
             Arrays.fill(d,-1);
         }
-        return solve(0,n,power);
+        return solve(1,n,n,x);
     }
-    private int solve(int idx,int sum,int[] power){
+    private int solve(int st,int n,int sum,int x){
         if(sum==0){
             return 1;
         }
-        if(idx>=power.length){
+        if(st>n){
             return 0;
         }
         if(sum<0){
             return 0;
         }
-        if(dp[idx][sum]!=-1){
-            return dp[idx][sum];
+        if(dp[st][sum]!=-1){
+            return dp[st][sum];
         }
-        int take=solve(idx+1,sum-power[idx],power);
-        int skip=solve(idx+1,sum,power);
-        dp[idx][sum] = (int)(((long)take + skip) % mod);
-        return dp[idx][sum];
+        int curr=(int)Math.pow(st,x);
+        int take=solve(st+1,n,sum-curr,x);
+        int skip=solve(st+1,n,sum,x);
+        dp[st][sum] = (int)(((long)take+skip)%mod);
+        return dp[st][sum];
     }
 }
