66762ab74debb3526de80709f5ac43259ffde198
Md Taufique <mdtaufique8084@gmail.com>
2025-08-27 19:53:25 +0530

    Time: 478 ms (84.21%) | Memory: 122.4 MB (25.00%) - LeetSync

--- 3733-length-of-longest-v-shaped-diagonal-segment/length-of-longest-v-shaped-diagonal-segment.java
+++ 3733-length-of-longest-v-shaped-diagonal-segment/length-of-longest-v-shaped-diagonal-segment.java
@@ -1,10 +1,21 @@
 class Solution {
     int[][] directions={{1,1},{1,-1},{-1,-1},{-1,1}};
     int m,n;
+    int[][][][] dp;
     public int lenOfVDiagonal(int[][] grid) {
         int res=0;
         m=grid.length;
         n=grid[0].length;
+        dp=new int[m][n][4][2];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                for(int d=0;d<4;d++){
+                    for(int c=0;c<2;c++){
+                        dp[i][j][d][c]=-1;
+                    }
+                }
+            }
+        }
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==1){
@@ -22,6 +33,9 @@ class Solution {
         if(i1<0 || i1>=m || j1<0 || j1>=n || grid[i1][j1]!=val){
             return 0;
         }
+        if(dp[i1][j1][d][canTurn]!=-1){
+            return dp[i1][j1][d][canTurn];
+        }
         int nextVal=val==2 ? 0 : 2;
         int length=0;
         int keepMovingStraight=1+solve(i1,j1,canTurn,grid,nextVal,d);
@@ -31,6 +45,6 @@ class Solution {
             int turnAround=Math.max(keepMovingStraight,1+solve(i1,j1,0,grid,nextVal,(d+1)%4));
             length=Math.max(length,turnAround);
         }
-        return length;
+        return dp[i1][j1][d][canTurn] = length;
     }
 }
