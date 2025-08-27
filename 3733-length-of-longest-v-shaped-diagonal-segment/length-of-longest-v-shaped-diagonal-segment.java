a53f0e24d72e0d473059cf11cb65a06fdef0d084
Md Taufique <mdtaufique8084@gmail.com>
2025-08-27 19:49:23 +0530

    Time: 1276 ms (6.58%) | Memory: 91.3 MB (40.79%) - LeetSync

---
+++ 3733-length-of-longest-v-shaped-diagonal-segment/length-of-longest-v-shaped-diagonal-segment.java
@@ -0,0 +1,36 @@
+class Solution {
+    int[][] directions={{1,1},{1,-1},{-1,-1},{-1,1}};
+    int m,n;
+    public int lenOfVDiagonal(int[][] grid) {
+        int res=0;
+        m=grid.length;
+        n=grid[0].length;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1){
+                    for(int d=0;d<4;d++){
+                        res=Math.max(res,1+solve(i,j,1,grid,2,d));
+                    }
+                }
+            }
+        }
+        return res;
+    }
+    private int solve(int i,int j,int canTurn,int[][] grid,int val,int d){
+        int i1=i+directions[d][0];
+        int j1=j+directions[d][1];
+        if(i1<0 || i1>=m || j1<0 || j1>=n || grid[i1][j1]!=val){
+            return 0;
+        }
+        int nextVal=val==2 ? 0 : 2;
+        int length=0;
+        int keepMovingStraight=1+solve(i1,j1,canTurn,grid,nextVal,d);
+        length=Math.max(length,keepMovingStraight);
+
+        if(canTurn==1){
+            int turnAround=Math.max(keepMovingStraight,1+solve(i1,j1,0,grid,nextVal,(d+1)%4));
+            length=Math.max(length,turnAround);
+        }
+        return length;
+    }
+}
