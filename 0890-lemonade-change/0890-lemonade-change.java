5d95a51edbd2f5846d6f4e704f58b7aa5e2b35ac
Md Taufique <mdtaufique8084@gmail.com>
2025-08-30 12:42:22 +0530

    Time: 2 ms (93.37%), Space: 56.2 MB (31.03%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.java
+++ 0890-lemonade-change/0890-lemonade-change.java
@@ -1,33 +1,26 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int bill : bills) {
-            if (bill == 5) {
-                // just take the 5
-                map.put(5, map.getOrDefault(5, 0) + 1);
-            } 
-            else if (bill == 10) {
-                int freq5 = map.getOrDefault(5, 0);
-                if (freq5 == 0) return false;
-                // give back one 5
-                map.put(5, freq5 - 1);
-                map.put(10, map.getOrDefault(10, 0) + 1);
-            } 
-            else { // bill == 20
-                int freq5 = map.getOrDefault(5, 0);
-                int freq10 = map.getOrDefault(10, 0);
-
-                if (freq10 > 0 && freq5 > 0) {
-                    // use one 10 and one 5
-                    map.put(10, freq10 - 1);
-                    map.put(5, freq5 - 1);
-                } else if (freq5 >= 3) {
-                    // use three 5s
-                    map.put(5, freq5 - 3);
-                } else {
+        int five=0,ten=0;
+        for(int bill:bills){
+            if(bill==5){
+                five++;
+            }
+            else if(bill==10){
+                if(five==0) return false;
+                five--;
+                ten++;
+            }
+            else{
+                if(ten>0 && five>0){
+                    five--;
+                    ten--;
+                }
+                else if(five>=3){
+                    five-=3;
+                }
+                else{
                     return false;
                 }
-                // we don't need to track 20s
             }
         }
         return true;
