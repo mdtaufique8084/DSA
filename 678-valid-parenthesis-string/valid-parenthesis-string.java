edd61a45168c79f907e1567802341ccba6b01ded
Md Taufique <mdtaufique8084@gmail.com>
2025-08-30 13:12:09 +0530

    Time: 1 ms (26.06%) | Memory: 43.3 MB (5.26%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -0,0 +1,35 @@
+class Solution {
+    int[][] dp;
+    public boolean checkValidString(String s) {
+        int n=s.length();
+        dp=new int[n+1][n+1];
+        for(int[] d:dp){
+            Arrays.fill(d,-1);
+        }
+        return solve(0,0,s,n);
+    }
+    private boolean solve(int idx,int open,String s,int n){
+        if(idx==n){
+            return open==0;
+        }
+        if(dp[idx][open]!=-1){
+            return dp[idx][open]==1;
+        }
+        boolean isValid=false;
+        if(s.charAt(idx)=='*'){
+            isValid |=solve(idx+1,open+1,s,n);
+            isValid |=solve(idx+1,open,s,n);
+            if(open>0){
+                isValid |=solve(idx+1,open-1,s,n);
+            }
+        }
+        else if(s.charAt(idx)=='('){
+            isValid |=solve(idx+1,open+1,s,n);
+        }
+        else if(open>0){
+            isValid |=solve(idx+1,open-1,s,n);
+        }
+        dp[idx][open]=isValid ? 1 : 0;
+        return isValid;
+    }
+}
