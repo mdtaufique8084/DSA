ab28e1394e4aa16261f1e51c9d38dc47145eda71
Md Taufique <mdtaufique8084@gmail.com>
2025-08-31 09:51:21 +0530

    Time: 49 ms (13.78%), Space: 45.7 MB (56.12%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,24 +1,16 @@
 class Solution {
-    int[] dp;
     public boolean canJump(int[] nums) {
         int n=nums.length;
-        dp=new int[n+1];
-        Arrays.fill(dp,-1);
-        return solve(nums,n,0);
-    }
-    private boolean solve(int[] nums,int n,int idx){
-        if(idx==n-1) return true;
-        if(idx>=n) return false;
-        if(dp[idx]!=-1){
-            return dp[idx]==1;
-        }
-        for(int i=1;i<=nums[idx];i++){
-            if(solve(nums,n,idx+i)){
-                dp[idx]=1;
-                return true;
+        boolean[] dp=new boolean[n];
+        dp[0]=true;
+        for(int i=1;i<n;i++){
+            for(int j=i-1;j>=0;j--){
+                if(dp[j] && j+nums[j]>=i){
+                    dp[i]=true;
+                    break;
+                }
             }
         }
-        dp[idx]=0;
-        return false;
+        return dp[n-1];
     }
 }
