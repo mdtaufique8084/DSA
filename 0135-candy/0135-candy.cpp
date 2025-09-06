9359ebf9ff0f26a7df44ffeb536cae074e75c946
Md Taufique <mdtaufique8084@gmail.com>
2025-09-06 15:29:34 +0530

    Time: 0 ms (100%), Space: 21.9 MB (95.55%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n=ratings.size();
+        int candy=n;
+        int i=1;
+        while(i<n){
+            if(ratings[i]==ratings[i-1]){
+                i++;
+                continue;
+            }
+            int peak=0;
+            while(ratings[i]>ratings[i-1]){
+                peak++;
+                candy+=peak;
+                i++;
+                if(i==n){
+                    return candy;
+                }
+            }
+            int dip=0;
+            while(i<n && ratings[i]<ratings[i-1]){
+                dip++;
+                candy+=dip;
+                i++;
+            }
+            candy-=min(dip,peak);
+        }
+        return candy;
+    }
+};
