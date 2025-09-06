852493ad4c4cf88e9ff67f1ca1b6478ed5d0d54b
Md Taufique <mdtaufique8084@gmail.com>
2025-09-06 23:53:07 +0530

    Time: 0 ms (100%), Space: 21.7 MB (45.06%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -2,21 +2,26 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         int i=0;
-        while(i<intervals.size()){
+        int n=intervals.size();
+        vector<vector<int>> result;
+        while(i<n){
             if(intervals[i][1]<newInterval[0]){
-                i++;
+                result.push_back(intervals[i]);
             }
             else if(intervals[i][0]>newInterval[1]){
-                intervals.insert(intervals.begin()+i,newInterval);
-                return intervals;
+                break;
             }
             else{
                 newInterval[0]=min(newInterval[0],intervals[i][0]);
                 newInterval[1]=max(newInterval[1],intervals[i][1]);
-                intervals.erase(intervals.begin()+i);
             }
+            i++;
         }
-        intervals.push_back(newInterval);
-        return intervals;
+        result.push_back(newInterval);
+        while(i<n){
+           result.push_back(intervals[i]);
+           i++; 
+        }
+        return result;
     }
 };
