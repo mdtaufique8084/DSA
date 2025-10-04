6d158b4696d80aa7360374a717c6f940cf8c594f
Md Taufique <mdtaufique8084@gmail.com>
2025-10-04 22:42:13 +0530

    Time: 1443 ms (25.55%), Space: 41.3 MB (59.82%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.java
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.java
@@ -1,24 +1,26 @@
 class Solution {
     public boolean canPartitionKSubsets(int[] nums, int k) {
-        int sum = 0;
-        for (int num : nums) sum += num;
-        if (sum % k != 0) return false;
-
-        int target = sum / k;
-        boolean[] used = new boolean[nums.length];
-        return backtrack(nums, k, 0, 0, target, used);
+        int sum=0;
+        for(int num:nums){
+            sum+=num;
+        }
+        if(sum%k!=0) return false;
+        int reqSum=sum/k;
+        int[] visited=new int[nums.length];
+        return solve(0,0,reqSum,k,nums,visited);
     }
-
-    private boolean backtrack(int[] nums, int k, int currSum, int start, int target, boolean[] used) {
-        if (k == 0) return true;
-        if (currSum == target) return backtrack(nums, k - 1, 0, 0, target, used);
-
-        for (int i = start; i < nums.length; i++) {
-            if (used[i] || currSum + nums[i] > target) continue;
-
-            used[i] = true;
-            if (backtrack(nums, k, currSum + nums[i], i + 1, target, used)) return true;
-            used[i] = false;
+    private boolean solve(int st,int currSum,int reqSum,int k,int[] nums,int[] visited){
+        if(k==0) return true;
+        if(currSum==reqSum){
+            return solve(0,0,reqSum,k-1,nums,visited); // new bucket will be formed
+        }
+        for(int i=st;i<nums.length;i++){
+            if(visited[i]==1 || currSum+nums[i]>reqSum) continue;
+            visited[i]=1;
+            if(solve(i+1,currSum+nums[i],reqSum,k,nums,visited)){ // if true from other bucket
+                return true;
+            }
+            visited[i]=0;
         }
         return false;
     }
