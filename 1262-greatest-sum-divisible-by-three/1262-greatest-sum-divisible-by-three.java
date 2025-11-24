0f8001182db807a36a1bdb7359c1c321f3c68c15
Md Taufique <mdtaufique8084@gmail.com>
2025-11-24 18:41:33 +0530

    Time: 20 ms (12.54%), Space: 52.9 MB (11.6%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.java
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.java
@@ -1,28 +1,32 @@
 class Solution {
-    Integer[][] dp;
-
     public int maxSumDivThree(int[] nums) {
-        int n = nums.length;
-        dp = new Integer[n][3];
-        return solve(nums, 0, 0);
+        List<Integer> rem1=new ArrayList<>();
+        List<Integer> rem2=new ArrayList<>();
+        int sum=0;
+        for(int num:nums){
+            sum+=num;
+            if(num%3==1){
+                rem1.add(num);
+            }
+            else if(num%3==2){
+                rem2.add(num);
+            }
+        }
+        if(sum%3==0) return sum;
+        Collections.sort(rem1);
+        Collections.sort(rem2);
+        int res=0;
+        if(sum%3==1){
+            int remove1=rem1.size()>=1 ? rem1.get(0) : Integer.MAX_VALUE;
+            int remove2=rem2.size()>=2 ? rem2.get(0)+rem2.get(1):Integer.MAX_VALUE;
+            res=sum-Math.min(remove1,remove2); 
+        }
+        else{
+            int remove2=rem2.size()>=1 ? rem2.get(0) : Integer.MAX_VALUE;
+            int remove1=rem1.size()>=2 ? rem1.get(0) +rem1.get(1):Integer.MAX_VALUE;
+            res=sum-Math.min(remove2,remove1);
+        }
+        return Math.max(res,0);
     }
 
-    private int solve(int[] nums, int idx, int rem) {
-        // BASE CASE
-        if (idx == nums.length) {
-            return rem == 0 ? 0 : Integer.MIN_VALUE;
-        }
-
-        if (dp[idx][rem] != null) {
-            return dp[idx][rem];
-        }
-
-        // INCLUDE nums[idx]
-        int take = nums[idx] + solve(nums, idx + 1, (rem + nums[idx] % 3) % 3);
-
-        // SKIP nums[idx]
-        int skip = solve(nums, idx + 1, rem);
-
-        return dp[idx][rem] = Math.max(take, skip);
-    }
 }
