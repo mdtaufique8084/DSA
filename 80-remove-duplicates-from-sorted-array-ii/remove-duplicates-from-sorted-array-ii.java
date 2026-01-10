fa5deb8e3f6efadfea549026a10181974cb296c0
Md Taufique <mdtaufique8084@gmail.com>
2026-01-10 20:05:09 +0530

    Time: 0 ms (100.00%) | Memory: 48.8 MB (31.85%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
@@ -1,18 +1,12 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int num:nums){
-            map.put(num,map.getOrDefault(num,0)+1);
-        }
-        int idx=0;
-        for(Map.Entry<Integer,Integer> entry: map.entrySet()){
-            int key=entry.getKey();
-            int count=Math.min(entry.getValue(),2);
-            for(int i=0;i<count;i++){
-                nums[idx++]=key;
+        int k=2;
+        for(int i=2;i<nums.length;i++){
+            if(nums[i]!=nums[k-2]){
+                nums[k]=nums[i];
+                k++;
             }
         }
-        Arrays.sort(nums,0,idx);
-        return idx;
+        return k;
     }
 }
