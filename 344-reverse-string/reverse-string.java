6c77e48a847a048882e253cc12cf68823cf51fe0
Md Taufique <mdtaufique8084@gmail.com>
2026-03-08 14:49:43 +0530

    Time: 0 ms (100.00%) | Memory: 48.1 MB (87.97%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -2,14 +2,11 @@ class Solution {
     public void reverseString(char[] s) {
         int i=0,j=s.length-1;
         while(i<j){
-            swap(i,j,s);
+            char temp=s[i];
+            s[i]=s[j];
+            s[j]=temp;
             i++;
             j--;
         }
-    }
-    private void swap(int a,int b,char[] s){
-        char temp=s[a];
-        s[a]=s[b];
-        s[b]=temp;
     }
 }
