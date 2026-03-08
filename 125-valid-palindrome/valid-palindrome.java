9e0caf581635bfc2a101c9c5071feb03b4e222ea
Md Taufique <mdtaufique8084@gmail.com>
2026-03-08 14:43:31 +0530

    Time: 3 ms (61.48%) | Memory: 44.3 MB (74.96%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,12 +1,15 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        if (s == null || s.isEmpty()) return true;
+        if(s.isEmpty() || s==null) return true; // empty & null string are always pallindrome
+        int n=s.length();
+        int i=0,j=n-1;
         s=s.toLowerCase();
-        int i=0,j=s.length()-1;
         while(i<j){
+            // condition to remove all the non alphanumeric character from left
             while(i<j && !Character.isLetterOrDigit(s.charAt(i))){
                 i++;
             }
+            // condition to remove all the non alphanumeric character from right
             while(i<j && !Character.isLetterOrDigit(s.charAt(j))){
                 j--;
             }
