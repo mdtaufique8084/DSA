56ccb6e6ae70f7d7f879578b18b21ee903eb4ac7
Md Taufique <mdtaufique8084@gmail.com>
2026-03-09 22:44:51 +0530

    Time: 1 ms (99.89%) | Memory: 47.6 MB (35.64%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,37 +1,46 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int n=nums.length;
-        int[] res=new int[n];
-        int left=-1,right=-1;
-        for(int i=0;i<n;i++){
-            if(nums[i]>0){
+        int n = nums.length;
+        int[] res = new int[n];
+        int left = -1, right = -1;
+
+        // finding the pivit index which is smallest negative
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
                 break;
             }
-            left++;
+            left = left + 1;
         }
-        right=left+1;
-        int idx=0;
-        while(left>=0 && right<n){
-            int x=nums[left]*nums[left];
-            int y=nums[right]*nums[right];
-            if(x>=y){
-                res[idx++]=y;
+        right = left + 1;
+        int idx = 0;
+
+        // merging two sorted array 
+        // array 1= sorted negative part 
+        // array 2=sorted positive part
+        while (left >= 0 && right < n) {
+            int sq1 = nums[left] * nums[left];
+            int sq2 = nums[right] * nums[right];
+            if (sq1 <= sq2) {
+                res[idx++] = sq1;
+                left--;
+            } else {
+                res[idx++] = sq2;
                 right++;
             }
-            else{
-                res[idx++]=x;
-                left--;
-            }
         }
+
+        // merging
 
         while(left>=0){
             res[idx++]=nums[left]*nums[left];
             left--;
         }
+
         while(right<n){
             res[idx++]=nums[right]*nums[right];
             right++;
         }
+
         return res;
     }
 }
