0f0ca22b8716ee9f7be6ef1b8e4dad25f19a61d7
Md Taufique <mdtaufique8084@gmail.com>
2026-03-14 00:36:37 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (94.46%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int n=nums.length;
+        int low=0,mid=0,high=n-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(low,mid,nums);
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                swap(mid,high,nums);
+                high--;
+            }
+        }
+    }
+    private void swap(int a,int b,int[] nums){
+        int temp=nums[a];
+        nums[a]=nums[b];
+        nums[b]=temp;
+    }
+}
