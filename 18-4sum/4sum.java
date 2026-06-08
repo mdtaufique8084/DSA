3343148bde6b9687c48f18734b7ade5123c4f12d
Md Taufique <mdtaufique8084@gmail.com>
2026-06-08 14:36:33 +0530

    Time: 22 ms (20.19%) | Memory: 45.9 MB (45.50%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -5,8 +5,8 @@ class Solution {
         List<List<Integer>> res=new ArrayList<>();
         for(int i=0;i<n-3;i++){
             if(i>0 && nums[i]==nums[i-1]) continue;
-            for(int j=i+1;j<n-2;j++){
-                if(j>i+1 && nums[j]==nums[j-1]) continue;
+            for(int j=i+1;j<n-2;){
+                // if(j>i+1 && nums[j]==nums[j-1]) continue;
                 int left=j+1;
                 int right=n-1;
                 while(left<right){
@@ -16,9 +16,9 @@ class Solution {
                         while(left<right && nums[left]==nums[left+1]){
                             left++;
                         }
-                        while(left<right && nums[right]==nums[right-1]){
-                            right--;
-                        }
+                        // while(left<right && nums[right]==nums[right-1]){
+                        //     right--;
+                        // }
                         left++;
                         right--;
                     }
@@ -28,6 +28,10 @@ class Solution {
                     else{
                         left++;
                     }
+                }
+                j++;
+                while(j<n && nums[j]==nums[j-1]){
+                    j++;
                 }
             }
         }
