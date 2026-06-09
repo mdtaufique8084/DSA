3110e3226155ff53fbb4e43b92f523cd0a27a619
Md Taufique <mdtaufique8084@gmail.com>
2026-06-09 23:23:52 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (49.93%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.java
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -1,27 +1,42 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        StringBuilder s1=new StringBuilder();
-        StringBuilder s2=new StringBuilder();
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            if(ch=='#' && !s1.isEmpty()){
-                s1.deleteCharAt(s1.length()-1);
+        int i=s.length()-1;
+        int j=t.length()-1;
+        int skipS=0,skipT=0;
+        while(i>=0 || j>=0){
+            while(i>=0){
+                if(s.charAt(i)=='#'){
+                    skipS++;
+                    i--;
+                }
+                else if(skipS>0){
+                    skipS--;
+                    i--;
+                }
+                else{
+                    break;
+                }
             }
-            if(ch!='#'){
-                s1.append(ch);
+            while(j>=0){
+                if(t.charAt(j)=='#'){
+                    skipT++;
+                    j--;
+                }
+                else if(skipT>0){
+                    skipT--;
+                    j--;
+                }
+                else{
+                    break;
+                }
             }
+            if(i>=0 && j>=0 && s.charAt(i)!=t.charAt(j)){
+                return false;
+            }
+            if ((i >= 0) != (j >= 0)) return false;
+            i--;
+            j--;
         }
-        for(int i=0;i<t.length();i++){
-            char ch=t.charAt(i);
-            if(ch=='#' && !s2.isEmpty()){
-                s2.deleteCharAt(s2.length()-1);
-            }
-            if(ch!='#'){
-                s2.append(ch);
-            }
-        }
-        System.out.println(s1.toString());
-        System.out.println(s2.toString());
-        return s1.toString().equals(s2.toString());
+        return true;
     }
 }
