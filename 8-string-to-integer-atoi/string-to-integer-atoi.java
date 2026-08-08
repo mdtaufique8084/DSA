9e73ffcafbfd7cf5c328f1d937b962014ce0bad8
Md Taufique <mdtaufique8084@gmail.com>
2026-08-08 22:58:58 +0530

    Time: 1 ms (100.00%) | Memory: 43.4 MB (95.09%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,41 +1,35 @@
 class Solution {
     public int myAtoi(String s) {
-        int start = 0;
-        int signed = 1;
-        int n = s.length();
-        long result = 0; // use long to detect overflow
+        int n=s.length();
+        int start=0;
+        int signed=1;
+        long res=0;
 
-        // 1. Skip leading spaces
-        while (start < n && s.charAt(start) == ' ') {
+        while(start<n && s.charAt(start)==' '){
             start++;
         }
 
-        // 2. Sign check
-        if (start < n && s.charAt(start) == '+') {
-            signed = 1;
+        if(start<n && s.charAt(start)=='+'){
+            signed=1;
             start++;
-        } else if (start < n && s.charAt(start) == '-') {
-            signed = -1;
+        }
+        else if (start<n && s.charAt(start)=='-'){
+            signed=-1;
             start++;
         }
 
-        // 3. Convert digits
-        for (int i = start; i < n; i++) {
-            char ch = s.charAt(i);
-            if (ch < '0' || ch > '9') break;
-
-            int digit = ch - '0';
-            result = result * 10 + digit;
-
-            // 4. Clamp on overflow
-            if (signed == 1 && result > Integer.MAX_VALUE) {
+        for(int i=start;i<n;i++){
+            char ch=s.charAt(i);
+            if(ch<'0' || ch>'9') break;
+            int digit=ch-'0';
+            res=res*10+digit;
+            if(signed==1 && res>Integer.MAX_VALUE){
                 return Integer.MAX_VALUE;
             }
-            if (signed == -1 && -result < Integer.MIN_VALUE) {
+            if(signed==-1 && -res<Integer.MIN_VALUE){
                 return Integer.MIN_VALUE;
             }
         }
-
-        return (int)(result * signed);
+        return (int)(signed*res);
     }
 }
