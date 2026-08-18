9b46e9feba534f5b0c472f76c800b3cc7eb2ea9f
Md Taufique <mdtaufique8084@gmail.com>
2026-08-18 06:38:59 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (54.02%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.java
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -1,39 +1,36 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        int i=s.length()-1;
-        int j=t.length()-1;
-        int skipS=0,skipT=0;
-        while(i>=0 || j>=0){
-            while(i>=0){
-                if(s.charAt(i)=='#'){
-                    skipS++;
+        int i = s.length() - 1;
+        int j = t.length() - 1;
+        int skip_s = 0, skip_t = 0;
+        while (i >= 0 || j >= 0) {
+            while (i >= 0) {
+                if (s.charAt(i) == '#') {
+                    skip_s++;
                     i--;
-                }
-                else if(skipS>0){
-                    skipS--;
+                } else if (skip_s > 0) {
+                    skip_s--;
                     i--;
-                }
-                else{
+                } else {
                     break;
                 }
             }
             while(j>=0){
                 if(t.charAt(j)=='#'){
-                    skipT++;
+                    skip_t++;
                     j--;
                 }
-                else if(skipT>0){
-                    skipT--;
+                else if(skip_t>0){
+                    skip_t--;
                     j--;
                 }
                 else{
                     break;
                 }
             }
-            if(i>=0 && j>=0 && s.charAt(i)!=t.charAt(j)){
-                return false;
-            }
-            if ((i >= 0) != (j >= 0)) return false;
+            char ch1=i<0 ? '$' : s.charAt(i);
+            char ch2=j<0 ? '$' : t.charAt(j);
+            if(ch1!=ch2) return false;
             i--;
             j--;
         }
