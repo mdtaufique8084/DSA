e4493eb70fcef5e62da06d535aaad352330840f3
Md Taufique <mdtaufique8084@gmail.com>
2026-09-22 23:07:57 +0530

    Time: 628 ms (7.32%) | Memory: 142.9 MB (64.55%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.java
+++ 187-repeated-dna-sequences/repeated-dna-sequences.java
@@ -1,13 +1,35 @@
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-        Set<String> seen =new HashSet<>();
-        Set<String> res =new HashSet<>();
-        for(int i=0;i<=s.length()-10;i++){
-            String part=s.substring(i,i+10);
-            if(seen.contains(part)){
-                res.add(part);
+
+        if (s.length() < 10) {
+            return new ArrayList<>();
+        }
+
+        int k = 10;
+        Set<Integer> seen = new HashSet<>();
+        Set<String> res = new HashSet<>();
+
+        int currSum = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('A', 0);
+        map.put('C', 1);
+        map.put('G', 2);
+        map.put('T', 3);
+        for(int i=0;i<k;i++){
+            int pow=k-1-i;
+            currSum=currSum+(int)Math.pow(4,pow)*map.get(s.charAt(i));
+        }
+        seen.add(currSum);
+
+        for(int i=k;i<s.length();i++){
+            currSum=currSum-(int)(Math.pow(4,k-1)*map.get(s.charAt(i-k)));
+            currSum=currSum*4;
+            currSum+=map.get(s.charAt(i));
+
+            if(seen.contains(currSum)){
+                res.add(s.substring(i-k+1,i+1));
             }
-            seen.add(part);
+            seen.add(currSum);
         }
         return new ArrayList<String>(res);
     }
